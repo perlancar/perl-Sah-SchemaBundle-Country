0296bb930550f5aa09956ffda4a12ea0c4090139
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-08-07 08:57:37 +0700

    [ux] Add summary of country name for country code completion answer

--- lib/Sah/Schema/country/code.pm
+++ lib/Sah/Schema/country/code.pm
@@ -1,5 +1,7 @@
 package Sah::Schema::country::code;
 
+use strict;
+
 # AUTHORITY
 # DATE
 # DIST
@@ -7,12 +9,23 @@ package Sah::Schema::country::code;
 
 use Locale::Codes::Country_Codes ();
 
-my $codes = [sort (
-    keys(%{ $Locale::Codes::Data{'country'}{'code2id'}{'alpha-2'} }),
-    keys(%{ $Locale::Codes::Data{'country'}{'code2id'}{'alpha-3'} }),
-)];
-die "Can't extract country codes from Locale::Codes::Country_Codes"
-    unless @$codes;
+my $codes = [];
+my $names = [];
+{
+    for my $alpha2 (keys(%{ $Locale::Codes::Data{'country'}{'code2id'}{'alpha-2'} })) {
+        push @$codes, $alpha2;
+        my $id = $Locale::Codes::Data{'country'}{'code2id'}{'alpha-2'}{$alpha2}[0];
+        push @$names, $Locale::Codes::Data{'country'}{'id2names'}{$id}[0];
+    }
+    for my $alpha3 (keys(%{ $Locale::Codes::Data{'country'}{'code2id'}{'alpha-3'} })) {
+        push @$codes, $alpha3;
+        my $id = $Locale::Codes::Data{'country'}{'code2id'}{'alpha-3'}{$alpha3}[0];
+        push @$names, $Locale::Codes::Data{'country'}{'id2names'}{$id}[0];
+    }
+
+    die "Can't extract country codes from Locale::Codes::Country_Codes"
+        unless @$codes;
+}
 
 our $schema = [str => {
     summary => 'Country code (alpha-2 or alpha-3)',
@@ -25,6 +38,7 @@ Code will be converted to lowercase.
 _
     match => '\A[a-z]{2,3}\z',
     in => $codes,
+    'x.in.summaries' => $names,
     'x.perl.coerce_rules' => ['From_str::to_lower'],
     examples => [
         {value=>'', valid=>0},
